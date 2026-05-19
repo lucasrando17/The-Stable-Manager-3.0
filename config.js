19a059ef5b55786d3a010f28ea7c8209f57d447d
lucasrando17 <luck.rando@gmail.com>
2026-05-19 21:27:08 +1000

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Replace the two values below in GitHub after upload.
 
 window.STABLE_MANAGER_CONFIG = {
-  SUPABASE_URL: "https://lnzqejouautpdbgaqqiz.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://lnzqejouautpdbgaqqiz.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_SsDDKUZo1g1C0RtBHR9dfw_cd6T1bMk"
 };
